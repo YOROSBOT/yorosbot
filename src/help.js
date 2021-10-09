18e725629e9350598a5a2467ea42fa41a916f38c
YOROSBOT <92189398+YOROSBOT@users.noreply.github.com>
2021-10-09 01:14:36 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -2,48 +2,48 @@ const  help  = ( prefixo ) => {
 	return  `
 
 
-*BOT CRIADO PELO ✧AӃ✫ƴ❍ℜ❍₷ 𓅂*
+ BOT CRIADO PELO ✧AӃ✫YOROS 
 
 FIGURINHAS: 
 
 *${prefix}sticker* ou *${prefix}stiker*
-( Envie ou responder imagem/gif/vídeo com legenda a *${prefix}sticker*\n )
+ Envie ou responder imagem/gif/vídeo com legenda a *${prefix}sticker*\n 
 *${prefix}toimg*
-( Responda a figurinha com *${prefix}toimg*\n ) 
+ Responda a figurinha com *${prefix}toimg*\n 
 
        GRUPOS 
 
 - O bot Yoros tem quer ser ADM do grupo. 
 
 *${prefix}welcome [1/0]*
-( Desc: Ativa o mode de boas vinda para o grupo ) 
+Desc: Ativa o mode de boas vinda para o grupo 
 
 *${prefix}add*
-( *${prefix} add 62813xxxxx* \n ) 
+ *${prefix} add 62813xxxxx* \n 
 
 *${prefix}kick*
-( *${prefix} kick @[membro do grupo]* \n ) 
+ *${prefix} kick @[membro do grupo]* \n 
 
 *${prefix}promote*
-( *${prefix}promote @[numero do membro]* \n ) 
+ *${prefix}promote @[numero do membro]* \n 
 
 *${prefix}demote*
-( *${prefix}demote @[mumero do membro]* \n ) 
+ *${prefix}demote @[mumero do membro]* \n 
 
 *${prefix}linkgroup*
-( Basta enviar o comando ) 
+Basta enviar o comando 
 
 *${prefix}leave*
-( Use este comando para o bot sair do grupo ) 
+ Use este comando para o bot sair do grupo 
 
 *${prefix}tagall*
-( Desc: Marca todos os membros do grupo, inclusive administradores ) 
+Desc: Marca todos os membros do grupo, inclusive administradores 
 
 *${prefix}tagall2*
-( Desc: Marca todos os membros do grupo, inclusive administradores ) 
+ Desc: Marca todos os membros do grupo, inclusive administradores 
 
 *${prefix}tagall3*
-( Marca todos os membros do grupo, inclusive administradores )
+Marca todos os membros do grupo, inclusive administradores 
 `
 } 
 
